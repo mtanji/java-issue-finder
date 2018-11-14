42168ef0eed4c18669a18c69f6f6d915e9017993
Mauricio Tanji <mauricio.tanji@aurea.com>
2018-11-14 08:32:32 -0200

    group and classify violations as severe, red and yellow in the list

---
+++ src/main/java/com/aurea/plugin/TrilogyBundle.java
@@ -0,0 +1,30 @@
+package com.aurea.plugin;
+
+import com.intellij.CommonBundle;
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.util.ResourceBundle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public class TrilogyBundle {
+    private static Reference<ResourceBundle> ourBundle;
+
+    public static final String BUNDLE = "messages.TrilogyBundle";
+
+    private TrilogyBundle() { }
+
+    public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+        return CommonBundle.message(getBundle(), key, params);
+    }
+
+    private static ResourceBundle getBundle() {
+        ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+        if (bundle == null) {
+            bundle = ResourceBundle.getBundle(BUNDLE);
+            ourBundle = new SoftReference<>(bundle);
+        }
+        return bundle;
+    }
+
+}
